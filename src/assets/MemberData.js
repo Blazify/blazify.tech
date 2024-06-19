0eb4c01ab4b38f76369390609203522122871b32
n0t-r0n1t <ronit.r@blazify.tech>
2024-06-19 15:37:40 +0530

    ✨ feat: deploy to ci

--- src/assets/MemberData.js
+++ src/assets/MemberData.js
@@ -1,42 +1,13 @@
 const data = [
   {
-    name: "Ronit Rahaman - CEO",
-    about:
-      "Student and software engineer with 5+ years of experience working for companies situated outside India.",
+    name: "Ronit Rahaman - Founder",
+    about: "Student and software engineer.",
     profileURL: "https://avatars.githubusercontent.com/u/92458476?v=4",
     tags: ["Web Development", "App Development", "Blockchain Development"],
     social: {
       github: "https://github.com/n0t-r0n1t",
       linkdin: "https://www.linkedin.com/in/ronitrahaman/",
       twitter: "https://twitter.com/romeahhh",
-    },
-  },
-
-  {
-    name: "Karim Islam - CFO & COO",
-    about: "Student with rich experience in financial management",
-    profileURL:
-      "https://media.discordapp.net/attachments/889433027153903636/1081631605342339204/Snapinsta.app_1080_81565710_483822332561372_6306561876327086781_n.jpg?width=809&height=1012",
-    tags: ["Accounting", "Financing", "Team Management"],
-    social: {
-      linkdin: "https://www.linkedin.com/in/karimislam",
-    },
-  },
-  {
-    name: "Ratul Saha - CTO",
-    about:
-      "Software Engineer with 4+ years of experience. Worked as freelancer and full-time developer for companies in India and abroad",
-    profileURL: "https://avatars.githubusercontent.com/u/53178362?v=4",
-    tags: [
-      "Web Development",
-      " Team Management",
-      "Decision making",
-      "Interviewing",
-    ],
-    social: {
-      github: "https://github.com/ThatAnonyG",
-      linkdin: "https://www.linkedin.com/in/ratuls/",
-      twitter: "https://twitter.com/ThatAnonyG",
     },
   },
 ];
